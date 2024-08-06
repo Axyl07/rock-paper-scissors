2a674cfa357c5ba99f500bb6854933eb032d9085
Axyl07 <aryanchauhan000777@gmail.com>
2024-08-07 00:01:25 +0530

    Update code to play single round

--- script.js
+++ script.js
@@ -10,17 +10,58 @@ function getComputerChoice(params) {
     else if (num>0.3&&num<0.6) {
         return "Paper";
     }
-    else if (num>0.6&&num<1) {
+    else {
         return "Scissors";
     }
 
 }
 
 
-function getHumanChoice(params) {
-    let input = prompt("What's your Choice");
-    return getComputerChoice();
+function getHumanChoice() {
+    let input = prompt("Enter One of the three choices : Rock,Paper,Scissors");
+    if (input==='Rock'||input==='Paper'||input==='Scissors') {
+        return input;   
+    }
+    else return "Enter a Valid Choice"
 }
 
-console.log(getHumanChoice());
-// console.log(getComputerChoice());
+
+
+function playRound(humanchoice,computerchoice) {
+    humanchoice = humanchoice.toUpperCase();
+    computerchoice = computerchoice.toUpperCase();
+    if (humanchoice===computerchoice) {
+        return "Its a tie!! Nobody gets a point"
+    }
+    else if (humanchoice === 'ROCK' && computerchoice === 'PAPER') {
+        computerScore++;
+        return "You Lose!!! Paper beats Rock"
+    }
+    else if (humanchoice === 'ROCK' && computerchoice === 'SCISSORS') {
+        humanScore++;
+        return "You Win!!! Rock beats Scissors"
+    }
+    else if (humanchoice === 'PAPER' && computerchoice === 'ROCK') {
+        humanScore++;
+        return "You Win!!! Paper beats Rock"
+    }
+    else if (humanchoice === 'PAPER' && computerchoice === 'SCISSORS') {
+        computerScore++;
+        return "You Lose!!! Scissor beats Paper"
+    }
+    else if (humanchoice === 'SCISSORS' && computerchoice === 'ROCK') {
+        computerScore++;
+        return "You Lose!!! Rock beats Scissors"
+    }
+    else {
+        humanScore++;
+        return "You Win!!! Scissors beats Paper"
+    }   
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("You Played " + humanSelection);
+console.log("Computer Played "+ computerSelection);
+console.log(playRound(humanSelection, computerSelection));
+
